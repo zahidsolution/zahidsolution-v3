2ff109c27e05c93d47942628010db64e7c1dfd3b
zahidsolution <zahidsolutions360@gmail.com>
2025-07-04 16:47:10 +0500

    Create app.py

---
+++ templates/templates/templates/templates/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        with open("feedback.txt", "a", encoding='utf-8') as f:
+            f.write(f"{name} said: {message}\n")
+        return redirect('/')
+    return render_template('feedback.html')
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
