ba1fd71d70326ff7a3807b1d55f90602b1f7c0d3
zahidsolution <zahidsolutions360@gmail.com>
2025-07-31 16:11:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,14 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/feedback', methods=['GET', 'POST'])
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/feedback')
 def feedback():
-    if request.method == 'POST':
-        name = request.form['name']
-        message = request.form['message']
-        with open("feedback.txt", "a", encoding='utf-8') as f:
-            f.write(f"{name} said: {message}\n")
-        return redirect('/')
     return render_template('feedback.html')
 
 if __name__ == '__main__':
