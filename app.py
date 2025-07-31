8ec976122720f8efabf44d43eb1db0ff017ac635
zahidsolution <zahidsolutions360@gmail.com>
2025-07-31 19:38:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,18 @@ init_db()
 def home():
     return render_template('index.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/portfolio')
+def portfolio_page():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     if request.method == 'POST':
